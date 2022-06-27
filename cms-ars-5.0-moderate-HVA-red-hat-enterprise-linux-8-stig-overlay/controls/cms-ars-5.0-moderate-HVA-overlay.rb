c9c2ebca9a4c7eb19f653e204fdfcf72fb2ee54a
Will Dower <wdower@mitre.org>
2022-06-27 12:04:45 -0400

    trying to make the overlay profiles chain off of each other
    Signed-off-by: Will Dower <wdower@mitre.org>

--- cms-ars-5.0-moderate-HVA-red-hat-enterprise-linux-8-stig-overlay/controls/cms-ars-5.0-moderate-HVA-overlay.rb
+++ cms-ars-5.0-moderate-HVA-red-hat-enterprise-linux-8-stig-overlay/controls/cms-ars-5.0-moderate-HVA-overlay.rb
@@ -1,5 +1,5 @@
 
-include_controls "redhat-enterprise-linux-8-stig-baseline" do
+include_controls "cms-ars-5.0-high-HVA-red-hat-enterprise-linux-8-stig-overlay" do
     control "SV-230222" do
         impact 0.0
         desc "caveat", "Mod HVA"
